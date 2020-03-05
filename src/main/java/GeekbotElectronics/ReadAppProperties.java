4e63c03ed7973eaba9b3a53076974451071e32de
Admin <limbert.vargas@fundacion-jala.org>
2020-03-05 10:34:53 -0400

    adding app properties to build a url

---
+++ src/main/java/GeekbotElectronics/ReadAppProperties.java
@@ -0,0 +1,39 @@
+package GeekbotElectronics;
+
+import core.selenium.utils.LoadProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class ReadAppProperties {
+    private static final String PROPERTIES_FILE = "geekbotelectronics.properties";
+    private static Map<String, String> mapPropertiesFile;
+    private static ReadAppProperties readAppProperties;
+    private static Properties properties;
+
+    private ReadAppProperties() {
+        initialize();
+    }
+
+    public static ReadAppProperties getInstance() {
+        if(readAppProperties == null) {
+            readAppProperties = new ReadAppProperties();
+        }
+        return readAppProperties;
+    }
+
+    private void initialize() {
+        mapPropertiesFile=new HashMap<>();
+        addGeekbotElectronicsProperties();
+    }
+
+    private void addGeekbotElectronicsProperties() {
+        properties = LoadProperties.readFile(PROPERTIES_FILE);
+        properties.forEach((key, value) -> mapPropertiesFile.put(key.toString(),value.toString()));
+    }
+
+    public Map<String, String> getAppProperties() {
+        return mapPropertiesFile;
+    }
+}

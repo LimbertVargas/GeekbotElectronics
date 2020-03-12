41265b768b198c1fd02de4d807056c658aa58622
LIMBERT <limbert.vargas@fundacion-jala.org>
2020-03-12 10:42:20 -0400

    adding steps

---
+++ src/test/java/steps/BornDateSteps.java
@@ -0,0 +1,10 @@
+package steps;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.When;
+
+public class BornDateSteps {
+    @And("the user adds a born date and the password")
+    public void addsABornDate() {
+    }
+}

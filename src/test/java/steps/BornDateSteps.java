f87e1c8fd9adaa6050a4c76ed8e111db3f095868
LIMBERT <limbert.vargas@fundacion-jala.org>
2020-03-12 13:02:38 -0400

    add last scenario

--- src/test/java/steps/BornDateSteps.java
+++ src/test/java/steps/BornDateSteps.java
@@ -1,16 +1,25 @@
 package steps;
 
 import cucumber.api.java.en.And;
-import cucumber.api.java.en.Then;
 import geekbotelectronics.ui.pages.AddBornDatePage;
+import geekbotelectronics.ui.pages.InformationPage;
 import geekbotelectronics.utils.GeekbotElectronicsConfig;
+import org.testng.Assert;
 
 public class BornDateSteps {
     private AddBornDatePage addBornDatePage;
+    private InformationPage informationPage;
+
     @And("the user adds a born date and the password")
     public void addsABornDate() {
         addBornDatePage = new AddBornDatePage();
         addBornDatePage.saveForm(GeekbotElectronicsConfig.getInstance().getCredentials("password"),
                 GeekbotElectronicsConfig.getInstance().getCredentials("bornDate"));
+    }
+
+    @And("the born date appears on information page")
+    public void bornDateAppears() {
+        informationPage = new InformationPage();
+        Assert.assertEquals(informationPage.getBornDate(),GeekbotElectronicsConfig.getInstance().getCredentials("bornDate"));
     }
 }
--- src/test/java/steps/CommonSteps.java
+++ src/test/java/steps/CommonSteps.java
@@ -3,15 +3,18 @@ package steps;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import geekbotelectronics.ui.PageTransporter;
+import geekbotelectronics.ui.pages.HeaderPage;
 
 public class CommonSteps {
+    private HeaderPage headerPage;
+
     @When("the user goes to (.*) page")
     public void loginPage(final String url) {
         PageTransporter.goToPage(url);
     }
 
-    @Then("and alert Informacion actualizada correctamente is displayed")
-    public void andAlertInformacionActualizadaCorrectamenteIsDisplayed() {
+    @Then("and alert (.*) is displayed")
+    public void alertIsDisplayed() {
 
     }
 }
